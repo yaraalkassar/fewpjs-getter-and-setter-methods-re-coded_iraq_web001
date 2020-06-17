1abf1a4d500cee25ae8fd602856b7f6732a21ef3
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-17 12:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ class Circle {
     radius = this.radius;
     pi = Math.PI;
   }
-
+  get diameter(){
+    return this.radius * 2
+  }
+  get circumference(){
+    return this.pi * this.diameter
+  }
+  get area(){
+    return (this.radius ** 2) *  this.pi
+  }
+  set diameter(diameter){
+    this.radius = diameter / 2
+  }
+  set circumference(circumference){
+    this.diameter = circumference /  this.pi
+  }
+  set area(area){
+    this.radius = Math.sqrt(area /  this.pi)
+  }
 }
